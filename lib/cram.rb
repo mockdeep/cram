fefbf3516754a5aa0b5af3c235c838872fa9d04a
Robert Fletcher <lobatifricha@gmail.com>
2024-02-11 14:12:48 -0800

    initial commit!

---
+++ lib/cram.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cram/version"
+
+module Cram
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cram_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cram do
+  it "has a version number" do
+    expect(Cram::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
