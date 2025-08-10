8738b493237579a27f217f950337bc9b3cc43402
Robert Fletcher <lobatifricha@gmail.com>
2025-08-10 10:46:23 -0700

    refactor study logic (#51)

--- lib/cram/models/deck.rb
+++ lib/cram/models/deck.rb
@@ -1,34 +1,42 @@
 class Cram::Models::Deck
-  attr_accessor :cards, :filepath, :target_success_ratio
+  attr_accessor :cards, :active_cards, :pending_cards, :practice_cards, :filepath, :target_success_ratio
 
   def initialize(filepath:)
-    @filepath = filepath
+    self.filepath = filepath
     YAML.safe_load_file(filepath, permitted_classes: [Symbol], symbolize_names: true) => { cards:, deck: }
 
     self.cards = cards.map.with_index do |card_data, index|
       Cram::Models::Card.new(**card_data, sequence: index + 1)
     end
 
-    @target_success_ratio = deck.fetch(:target_success_ratio)
+    self.target_success_ratio = deck.fetch(:target_success_ratio)
+    touch
   end
 
   def name
     File.basename(filepath, '.yml').titleize
   end
 
-  def practice_cards
-    active_cards.select do |card|
-      card.success_ratio < target_success_ratio ||
-        card.review_threshold < active_cards.count
-    end
+  def active_count
+    active_cards.count
   end
 
-  def active_cards
-    cards.select(&:active?)
+  def pending_count
+    pending_cards.count
   end
 
-  def pending_cards
-    cards.reject(&:active?)
+  def practice_count
+    practice_cards.count
+  end
+
+  def touch
+    self.active_cards, self.pending_cards = cards.partition(&:active?)
+    self.practice_cards =
+      active_cards.select do |card|
+        card.success_ratio < target_success_ratio ||
+          card.review_threshold < active_cards.count
+      end
+    File.write(filepath, to_yaml)
   end
 
   def to_yaml
